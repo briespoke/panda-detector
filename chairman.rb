921a731e804b3277a7adebdbc1f665d4b9cf8614
Brian Murphy <wavenger@gmail.com>
2012-12-04 04:22:24 +0000

    Fully functional!

--- chairman.rb
+++ chairman.rb
@@ -4,6 +4,10 @@ require 'json'
 require 'time'
 require 'cgi'
 
+PANDA_LOCATION="UN Plaza"
+PANDA_WINDOW=19
+PANDA_USERNAME="chairmantruck"
+
 def http_get(domain,path,params)
   return Net::HTTP.get(domain, "#{path}?".concat(params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&'))) if not params.nil?
   return Net::HTTP.get(domain, path)
@@ -14,16 +18,14 @@ def twoxy_get(url)
 end
 
 def is_chairman_nearby?
-  chairman_json = twoxy_get('http://twitter.com/statuses/user_timeline/chairmantruck.json')
+  chairman_json = twoxy_get('http://twitter.com/statuses/user_timeline/' + PANDA_USERNAME + '.json')
 
   chairman_json.each do |tweet|
-    # puts JSON.pretty_generate tweet
-    # puts tweet["created_at"] + tweet["text"]
-    if tweet["text"].match(Regexp.new('Hayward', Regexp::IGNORECASE)) != nil
+    if tweet["text"].match(Regexp.new(PANDA_LOCATION, Regexp::IGNORECASE)) != nil
       tweet_time = Time.parse tweet["created_at"]
       now = Time.now
       
-      if tweet_time + (19 * 60 * 60) > now
+      if tweet_time + (PANDA_WINDOW * 60 * 60) > now
         return true
       end
     end
@@ -32,11 +34,13 @@ def is_chairman_nearby?
 end
 
 def turn_kitten_off
-  puts "Kitten go to sleep!"
+  `gpio -g write 4 0`
+  puts "Kitty go to sleep!"
 end
 
 def turn_kitten_on
-  puts "Kitten is awake!"
+  `gpio -g write 4 1`
+  puts "Kitty want a sandwich!"
 end
 
 while true
